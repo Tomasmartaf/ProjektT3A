eeb0892d568ee5a8a12801c44b26af241374878f
MartinekTomáš <tomas.martinek@frengp.cz>
2026-04-07 08:30:25 +0200

    Přidání diakritiky do menu tlačítek, změněný výběr slova a přepsání na podtržítka

--- Projekt PRG TM/herniOkno.Designer.cs
+++ Projekt PRG TM/herniOkno.Designer.cs
@@ -59,6 +59,13 @@
             btnY = new Button();
             btnZ = new Button();
             btnStart = new Button();
+            btnCc = new Button();
+            btnDd = new Button();
+            btnNn = new Button();
+            btnRr = new Button();
+            btnSs = new Button();
+            btnTt = new Button();
+            btnZz = new Button();
             ((System.ComponentModel.ISupportInitialize)obesenecKresleni).BeginInit();
             flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
@@ -100,7 +107,9 @@
             flowLayoutPanel1.Controls.Add(btnA);
             flowLayoutPanel1.Controls.Add(btnB);
             flowLayoutPanel1.Controls.Add(btnC);
+            flowLayoutPanel1.Controls.Add(btnCc);
             flowLayoutPanel1.Controls.Add(btnD);
+            flowLayoutPanel1.Controls.Add(btnDd);
             flowLayoutPanel1.Controls.Add(btnE);
             flowLayoutPanel1.Controls.Add(btnF);
             flowLayoutPanel1.Controls.Add(btnG);
@@ -111,18 +120,23 @@
             flowLayoutPanel1.Controls.Add(btnL);
             flowLayoutPanel1.Controls.Add(btnM);
             flowLayoutPanel1.Controls.Add(btnN);
+            flowLayoutPanel1.Controls.Add(btnNn);
             flowLayoutPanel1.Controls.Add(btnO);
             flowLayoutPanel1.Controls.Add(btnP);
             flowLayoutPanel1.Controls.Add(btnQ);
             flowLayoutPanel1.Controls.Add(btnR);
+            flowLayoutPanel1.Controls.Add(btnRr);
             flowLayoutPanel1.Controls.Add(btnS);
+            flowLayoutPanel1.Controls.Add(btnSs);
             flowLayoutPanel1.Controls.Add(btnT);
+            flowLayoutPanel1.Controls.Add(btnTt);
             flowLayoutPanel1.Controls.Add(btnU);
             flowLayoutPanel1.Controls.Add(btnV);
             flowLayoutPanel1.Controls.Add(btnW);
             flowLayoutPanel1.Controls.Add(btnX);
             flowLayoutPanel1.Controls.Add(btnY);
             flowLayoutPanel1.Controls.Add(btnZ);
+            flowLayoutPanel1.Controls.Add(btnZz);
             flowLayoutPanel1.Location = new Point(22, 85);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
             flowLayoutPanel1.Size = new Size(194, 241);
@@ -169,7 +183,7 @@
             btnD.BackColor = Color.Black;
             btnD.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnD.ForeColor = Color.White;
-            btnD.Location = new Point(96, 3);
+            btnD.Location = new Point(127, 3);
             btnD.Name = "btnD";
             btnD.Size = new Size(25, 32);
             btnD.TabIndex = 3;
@@ -181,7 +195,7 @@
             btnE.BackColor = Color.Black;
             btnE.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnE.ForeColor = Color.White;
-            btnE.Location = new Point(127, 3);
+            btnE.Location = new Point(3, 41);
             btnE.Name = "btnE";
             btnE.Size = new Size(25, 32);
             btnE.TabIndex = 4;
@@ -193,7 +207,7 @@
             btnF.BackColor = Color.Black;
             btnF.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnF.ForeColor = Color.White;
-            btnF.Location = new Point(158, 3);
+            btnF.Location = new Point(34, 41);
             btnF.Name = "btnF";
             btnF.Size = new Size(25, 32);
             btnF.TabIndex = 5;
@@ -205,7 +219,7 @@
             btnG.BackColor = Color.Black;
             btnG.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnG.ForeColor = Color.White;
-            btnG.Location = new Point(3, 41);
+            btnG.Location = new Point(65, 41);
             btnG.Name = "btnG";
             btnG.Size = new Size(25, 32);
             btnG.TabIndex = 6;
@@ -217,7 +231,7 @@
             btnH.BackColor = Color.Black;
             btnH.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnH.ForeColor = Color.White;
-            btnH.Location = new Point(34, 41);
+            btnH.Location = new Point(96, 41);
             btnH.Name = "btnH";
             btnH.Size = new Size(25, 32);
             btnH.TabIndex = 7;
@@ -229,7 +243,7 @@
             btnI.BackColor = Color.Black;
             btnI.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnI.ForeColor = Color.White;
-            btnI.Location = new Point(65, 41);
+            btnI.Location = new Point(127, 41);
             btnI.Name = "btnI";
             btnI.Size = new Size(25, 32);
             btnI.TabIndex = 8;
@@ -241,7 +255,7 @@
             btnJ.BackColor = Color.Black;
             btnJ.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnJ.ForeColor = Color.White;
-            btnJ.Location = new Point(96, 41);
+            btnJ.Location = new Point(158, 41);
             btnJ.Name = "btnJ";
             btnJ.Size = new Size(25, 32);
             btnJ.TabIndex = 9;
@@ -253,7 +267,7 @@
             btnK.BackColor = Color.Black;
             btnK.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnK.ForeColor = Color.White;
-            btnK.Location = new Point(127, 41);
+            btnK.Location = new Point(3, 79);
             btnK.Name = "btnK";
             btnK.Size = new Size(25, 32);
             btnK.TabIndex = 10;
@@ -265,7 +279,7 @@
             btnL.BackColor = Color.Black;
             btnL.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnL.ForeColor = Color.White;
-            btnL.Location = new Point(158, 41);
+            btnL.Location = new Point(34, 79);
             btnL.Name = "btnL";
             btnL.Size = new Size(25, 32);
             btnL.TabIndex = 11;
@@ -277,7 +291,7 @@
             btnM.BackColor = Color.Black;
             btnM.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnM.ForeColor = Color.White;
-            btnM.Location = new Point(3, 79);
+            btnM.Location = new Point(65, 79);
             btnM.Name = "btnM";
             btnM.Size = new Size(25, 32);
             btnM.TabIndex = 12;
@@ -289,7 +303,7 @@
             btnN.BackColor = Color.Black;
             btnN.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnN.ForeColor = Color.White;
-            btnN.Location = new Point(34, 79);
+            btnN.Location = new Point(96, 79);
             btnN.Name = "btnN";
             btnN.Size = new Size(25, 32);
             btnN.TabIndex = 13;
@@ -301,7 +315,7 @@
             btnO.BackColor = Color.Black;
             btnO.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnO.ForeColor = Color.White;
-            btnO.Location = new Point(65, 79);
+            btnO.Location = new Point(158, 79);
             btnO.Name = "btnO";
             btnO.Size = new Size(25, 32);
             btnO.TabIndex = 14;
@@ -313,7 +327,7 @@
             btnP.BackColor = Color.Black;
             btnP.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnP.ForeColor = Color.White;
-            btnP.Location = new Point(96, 79);
+            btnP.Location = new Point(3, 117);
             btnP.Name = "btnP";
             btnP.Size = new Size(25, 32);
             btnP.TabIndex = 15;
@@ -325,7 +339,7 @@
             btnQ.BackColor = Color.Black;
             btnQ.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnQ.ForeColor = Color.White;
-            btnQ.Location = new Point(127, 79);
+            btnQ.Location = new Point(34, 117);
             btnQ.Name = "btnQ";
             btnQ.Size = new Size(25, 32);
             btnQ.TabIndex = 16;
@@ -337,7 +351,7 @@
             btnR.BackColor = Color.Black;
             btnR.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnR.ForeColor = Color.White;
-            btnR.Location = new Point(158, 79);
+            btnR.Location = new Point(65, 117);
             btnR.Name = "btnR";
             btnR.Size = new Size(25, 32);
             btnR.TabIndex = 17;
@@ -349,7 +363,7 @@
             btnS.BackColor = Color.Black;
             btnS.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnS.ForeColor = Color.White;
-            btnS.Location = new Point(3, 117);
+            btnS.Location = new Point(127, 117);
             btnS.Name = "btnS";
             btnS.Size = new Size(25, 32);
             btnS.TabIndex = 18;
@@ -361,7 +375,7 @@
             btnT.BackColor = Color.Black;
             btnT.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnT.ForeColor = Color.White;
-            btnT.Location = new Point(34, 117);
+            btnT.Location = new Point(3, 155);
             btnT.Name = "btnT";
             btnT.Size = new Size(25, 32);
             btnT.TabIndex = 19;
@@ -373,7 +387,7 @@
             btnU.BackColor = Color.Black;
             btnU.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnU.ForeColor = Color.White;
-            btnU.Location = new Point(65, 117);
+            btnU.Location = new Point(65, 155);
             btnU.Name = "btnU";
             btnU.Size = new Size(25, 32);
             btnU.TabIndex = 20;
@@ -385,7 +399,7 @@
             btnV.BackColor = Color.Black;
             btnV.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnV.ForeColor = Color.White;
-            btnV.Location = new Point(96, 117);
+            btnV.Location = new Point(96, 155);
             btnV.Name = "btnV";
             btnV.Size = new Size(25, 32);
             btnV.TabIndex = 21;
@@ -397,7 +411,7 @@
             btnW.BackColor = Color.Black;
             btnW.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnW.ForeColor = Color.White;
-            btnW.Location = new Point(127, 117);
+            btnW.Location = new Point(127, 155);
             btnW.Name = "btnW";
             btnW.Size = new Size(25, 32);
             btnW.TabIndex = 22;
@@ -409,7 +423,7 @@
             btnX.BackColor = Color.Black;
             btnX.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnX.ForeColor = Color.White;
-            btnX.Location = new Point(158, 117);
+            btnX.Location = new Point(158, 155);
             btnX.Name = "btnX";
             btnX.Size = new Size(25, 32);
             btnX.TabIndex = 23;
@@ -421,7 +435,7 @@
             btnY.BackColor = Color.Black;
             btnY.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnY.ForeColor = Color.White;
-            btnY.Location = new Point(3, 155);
+            btnY.Location = new Point(3, 193);
             btnY.Name = "btnY";
             btnY.Size = new Size(25, 32);
             btnY.TabIndex = 24;
@@ -433,7 +447,7 @@
             btnZ.BackColor = Color.Black;
             btnZ.Font = new Font("Consolas", 9F, FontStyle.Bold);
             btnZ.ForeColor = Color.White;
-            btnZ.Location = new Point(34, 155);
+            btnZ.Location = new Point(34, 193);
             btnZ.Name = "btnZ";
             btnZ.Size = new Size(25, 32);
             btnZ.TabIndex = 25;
@@ -452,6 +466,90 @@
             btnStart.Text = "START";
             btnStart.UseVisualStyleBackColor = false;
             btnStart.Click += btnStart_Click;
+            // 
+            // btnCc
+            // 
+            btnCc.BackColor = Color.Black;
+            btnCc.Font = new Font("Consolas", 9F, FontStyle.Bold);
+            btnCc.ForeColor = Color.White;
+            btnCc.Location = new Point(96, 3);
+            btnCc.Name = "btnCc";
+            btnCc.Size = new Size(25, 32);
+            btnCc.TabIndex = 26;
+            btnCc.Text = "Č";
+            btnCc.UseVisualStyleBackColor = false;
+            // 
+            // btnDd
+            // 
+            btnDd.BackColor = Color.Black;
+            btnDd.Font = new Font("Consolas", 9F, FontStyle.Bold);
+            btnDd.ForeColor = Color.White;
+            btnDd.Location = new Point(158, 3);
+            btnDd.Name = "btnDd";
+            btnDd.Size = new Size(25, 32);
+            btnDd.TabIndex = 27;
+            btnDd.Text = "Ď";
+            btnDd.UseVisualStyleBackColor = false;
+            // 
+            // btnNn
+            // 
+            btnNn.BackColor = Color.Black;
+            btnNn.Font = new Font("Consolas", 9F, FontStyle.Bold);
+            btnNn.ForeColor = Color.White;
+            btnNn.Location = new Point(127, 79);
+            btnNn.Name = "btnNn";
+            btnNn.Size = new Size(25, 32);
+            btnNn.TabIndex = 28;
+            btnNn.Text = "Ň";
+            btnNn.UseVisualStyleBackColor = false;
+            // 
+            // btnRr
+            // 
+            btnRr.BackColor = Color.Black;
+            btnRr.Font = new Font("Consolas", 9F, FontStyle.Bold);
+            btnRr.ForeColor = Color.White;
+            btnRr.Location = new Point(96, 117);
+            btnRr.Name = "btnRr";
+            btnRr.Size = new Size(25, 32);
+            btnRr.TabIndex = 29;
+            btnRr.Text = "Ř";
+            btnRr.UseVisualStyleBackColor = false;
+            // 
+            // btnSs
+            // 
+            btnSs.BackColor = Color.Black;
+            btnSs.Font = new Font("Consolas", 9F, FontStyle.Bold);
+            btnSs.ForeColor = Color.White;
+            btnSs.Location = new Point(158, 117);
+            btnSs.Name = "btnSs";
+            btnSs.Size = new Size(25, 32);
+            btnSs.TabIndex = 30;
+            btnSs.Text = "Š";
+            btnSs.UseVisualStyleBackColor = false;
+            // 
+            // btnTt
+            // 
+            btnTt.BackColor = Color.Black;
+            btnTt.Font = new Font("Consolas", 9F, FontStyle.Bold);
+            btnTt.ForeColor = Color.White;
+            btnTt.Location = new Point(34, 155);
+            btnTt.Name = "btnTt";
+            btnTt.Size = new Size(25, 32);
+            btnTt.TabIndex = 31;
+            btnTt.Text = "Ť";
+            btnTt.UseVisualStyleBackColor = false;
+            // 
+            // btnZz
+            // 
+            btnZz.BackColor = Color.Black;
+            btnZz.Font = new Font("Consolas", 9F, FontStyle.Bold);
+            btnZz.ForeColor = Color.White;
+            btnZz.Location = new Point(65, 193);
+            btnZz.Name = "btnZz";
+            btnZz.Size = new Size(25, 32);
+            btnZz.TabIndex = 32;
+            btnZz.Text = "Ž";
+            btnZz.UseVisualStyleBackColor = false;
             // 
             // herniOkno
             // 
@@ -505,5 +603,12 @@
         private Button btnZ;
         private Button btnA;
         private Button btnStart;
+        private Button btnCc;
+        private Button btnDd;
+        private Button btnNn;
+        private Button btnRr;
+        private Button btnSs;
+        private Button btnTt;
+        private Button btnZz;
     }
 }
--- Projekt PRG TM/herniOkno.cs
+++ Projekt PRG TM/herniOkno.cs
@@ -1,15 +1,23 @@
-﻿namespace Projekt_PRG_TM
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Projekt_PRG_TM
 {
     public partial class herniOkno : Form
     {
         bool hraje = false;
         Random rnd = new Random();
-        int slovoCislo;
 
-        string[] slovaPole = {
-        "jablko", "strom", "auto", "pes", "kočka",
-        "škola", "program", "hra", "kniha", "mobil",
-        "řeka", "hora", "město", "počítač", "slunce"
+        string vybraneSlovo;
+        char[] hadaneZnaky;
+        int randomSlovo;
+
+        string[] slovaPole =
+        {
+            "jablko", "strom", "auto", "pes", "kočka",
+            "škola", "program", "hra", "kniha", "mobil",
+            "řeka", "hora", "město", "počítač", "slunce"
         };
 
         public herniOkno()
@@ -17,6 +25,22 @@
             InitializeComponent();
         }
 
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            hraje = true;
+            int randomSlovo = rnd.Next(slovaPole.Length);
+
+            vybraneSlovo = slovaPole[randomSlovo];
+
+            hadaneZnaky = new char[vybraneSlovo.Length];
+            for (int i = 0; i < hadaneZnaky.Length; i++)
+            {
+                hadaneZnaky[i] = '_';
+            }
+
+            hadaneSlovo.Text = string.Join(" ", hadaneZnaky);
+        }
+        
         private void btnKonecHerniOkno_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -26,17 +50,5 @@
         {
             this.Hide();
         }
-
-        private void btnStart_Click(object sender, EventArgs e)
-        {
-            hraje = true;
-            slovoCislo = rnd.Next(0, slovaPole.Length);
-
-            string vybraneSlovo = slovaPole[slovoCislo];
-            string podtrzitka = string.Join(" ", vybraneSlovo.Select(c => '_'));
-
-            hadaneSlovo.Text = podtrzitka;
-        }
-    
     }
 }
